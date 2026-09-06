5450176baea7816585dcf7bdc0eda67ff53d67d4
Adam Czeponis <tarantulaadam@gmail.com>
2026-09-06 20:31:44 +0200

    Add files via upload

---
+++ ROZDZIAŁ I/1.2/1.2 Debugger.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Podaj swoje imię: ");
+string imie = Console.ReadLine();
+
+Console.Write("Podaj swój wiek: ");
+int wiek = Console.ReadLine();
+
+Console.WriteLine("Cześć {imie}!");
+
+int wiekZaRok = wiek + 1;
+console.WriteLine($"Za rok będziesz mieć {wiekZaRok} lat.");
+
+
+
+
