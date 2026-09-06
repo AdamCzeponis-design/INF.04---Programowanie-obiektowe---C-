41aecdd4341fe8562aad78c1f915c327c54a5074
Adam Czeponis <tarantulaadam@gmail.com>
2026-09-06 20:22:57 +0200

    Add files via upload

---
+++ ROZDZIAŁ I/1.2/1.2 Konwertowanie.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Podaj rok swojego urodzenia: ");
+string rokTekst = Console.ReadLine();
+
+// Konwersja ze string na int
+int rokUrodzenia = int.Parse(rokTekst);
+
+// Skrócony zapis (często używany w praktyce):
+// int rokUrodzenia = int.Parse(Console.ReadLine());
+
+int wiek = 2026 - rokUrodzenia;
+Console.WriteLine($"W tym roku kończysz {wiek} lat!");
+
+
---
+++ ROZDZIAŁ I/1.2/1.2 Odczyt danych.cs
@@ -0,0 +1,10 @@
+﻿// ReadLine – pobiera cały tekst
+Console.Write("Wpisz swoje miasto: ");
+string miasto = Console.ReadLine(); // np. "Warszawa"
+Console.WriteLine(miasto);
+
+// Read – pobiera kod pierwszego wpisanego znaku
+Console.Write("Wpisz jedną literę: ");
+int kodZnaku = Console.Read(); // jeśli wpiszesz "A", zmienna otrzyma wartość 65
+Console.WriteLine(kodZnaku);
+
